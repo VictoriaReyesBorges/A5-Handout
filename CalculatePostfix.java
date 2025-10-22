5896b7e60b5073cd0a162586c52dba4c18298cd6
kiaras-create <ksunilsingh@smith.edu>
2025-10-22 17:45:59 -0400

    added space in print statement

--- CalculatePostfix.java
+++ CalculatePostfix.java
@@ -51,52 +51,52 @@ public static Double postfixToResult(Queue<Object> tokens) {
 
 
 
-    /**
-     * Applies the operator to operands a and b and retuns the result
-     * 
-     * @param a the left operand
-     * @param b the right operand
-     * @param op the operators (+, -, *, /, or ^)
-     * @return the result of applying the operator to a and b
-     * @throws IllegalArgumentException for division by 0 and unsupported operator
-     */
-    public static double applyOperator(double a, double b, char op) {
-        if (op == '+') {
-            return a + b;
-        } else if (op == '-') {
-            return a - b;
-        } else if (op == '*') {
-            return a * b;
-        } else if (op == '/') {
-            if (b == 0.0) {
-                throw new IllegalArgumentException("Can't be divided by 0");
-            }
-            return a / b;
-        } else if (op == '^') {
-            return Math.pow (a, b);
-        } else {
-            throw new IllegalArgumentException("Unsupported Operator:" + op);
-        }
-    }
+    // /**
+    //  * Applies the operator to operands a and b and retuns the result
+    //  * 
+    //  * @param a the left operand
+    //  * @param b the right operand
+    //  * @param op the operators (+, -, *, /, or ^)
+    //  * @return the result of applying the operator to a and b
+    //  * @throws IllegalArgumentException for division by 0 and unsupported operator
+    //  */
+    // public static double applyOperator(double a, double b, char op) {
+    //     if (op == '+') {
+    //         return a + b;
+    //     } else if (op == '-') {
+    //         return a - b;
+    //     } else if (op == '*') {
+    //         return a * b;
+    //     } else if (op == '/') {
+    //         if (b == 0.0) {
+    //             throw new IllegalArgumentException("Can't be divided by 0");
+    //         }
+    //         return a / b;
+    //     } else if (op == '^') {
+    //         return Math.pow (a, b);
+    //     } else {
+    //         throw new IllegalArgumentException("Unsupported Operator:" + op);
+    //     }
+    // }
 
-    /**
-     * Returns the precedence level
-     * 
-     * @param op the operators (+, -, *, /, or ^)
-     * @return precedence level
-     * @throws IllegalArgumentException for unsupported operator
-     */
-    public static int precedence(char op) {
-        if (op == '+' || op == '-') {
-            return 1;
-        } else if (op == '*' || op == '/') {
-            return 2;
-        } else if (op == '^') {
-            return 3;
-        } else {
-            throw new IllegalArgumentException("Unsupported OPerator:" + op);
-        }
-    }
+    // /**
+    //  * Returns the precedence level
+    //  * 
+    //  * @param op the operators (+, -, *, /, or ^)
+    //  * @return precedence level
+    //  * @throws IllegalArgumentException for unsupported operator
+    //  */
+    // public static int precedence(char op) {
+    //     if (op == '+' || op == '-') {
+    //         return 1;
+    //     } else if (op == '*' || op == '/') {
+    //         return 2;
+    //     } else if (op == '^') {
+    //         return 3;
+    //     } else {
+    //         throw new IllegalArgumentException("Unsupported OPerator:" + op);
+    //     }
+    // }
 
        public static void main(String[] args) {
        String expression = args[0];
@@ -108,6 +108,6 @@ public static Double postfixToResult(Queue<Object> tokens) {
        double result = postfixToResult(tokens);
 
 
-       System.out.println("Answer" + result);
+       System.out.println("Answer" + " " + result);
    }
 }
