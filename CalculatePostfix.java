912039802c60b2516a753b32092a60cf1d610d8d
kiaras-create <ksunilsingh@smith.edu>
2025-10-21 22:31:58 -0400

    Most of postFixtToResult done

--- CalculatePostfix.java
+++ CalculatePostfix.java
@@ -1,5 +1,47 @@
 public class CalculatePostfix {
     public static Double postfixToResult(Queue<Object> tokens) {
-        // FILL IN
+        Stack<Double> stack = new Stack<>();
+        while (!tokens.isEmpty()){
+            Object token = tokens.remove();
+            if (token instanceof Double){
+                stack.push((Double) token);
+
+            } else if (token instanceof Character){
+                char operation = (Character) token;
+
+                double b = stack.pop();
+                double a = stack.pop();
+                double result = 0.0;
+
+                if (operation == '+'){
+                    result = a + b;
+            }
+                else if (operation == '-'){
+                    result = a - b;
+                }
+                
+                else if (operation == '*'){
+                    result = a * b;
+                }
+
+                else if (operation == '/'){
+                    if (b == 0){
+                        throw new IllegalArgumentException("Division by zero");
+                    } else {
+                        result = a / b;
+                    }
+                }
+            stack.push(result);
+            }
+
+            
+
+
+
+        }
+        return stack.pop();
     }
+
+    
+  
 }
